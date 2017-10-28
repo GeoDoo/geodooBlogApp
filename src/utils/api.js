f43cc6aa7824f82c2003bee05298333ce4d1b95e
Georgios Karametas <geometas@hotmail.com>
2017-10-28 19:42:05 +0300

    Updated the https in the api calls

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,10 +1,10 @@
 const api = {
 	fetchSiteInfo() {
-		let url = 'http://geodoo.work/wp-json';
+		let url = 'https://geodoo.work/wp-json';
 		return fetch(url).then((res) => res.json()); 
 	},
 	fetchPosts() {
-		let url = 'http://geodoo.work/wp-json/wp/v2/posts';
+		let url = 'https://geodoo.work/wp-json/wp/v2/posts';
 		return fetch(url).then((res) => res.json());
 	}
 };
