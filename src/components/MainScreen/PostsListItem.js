7318ae1f59f940822147d3b3292af8213b08ca8b
Georgios Karametas <geometas@hotmail.com>
2017-10-29 16:26:28 +0200

    Minor changes

--- src/components/MainScreen/PostsListItem.js
+++ src/components/MainScreen/PostsListItem.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import { View, Text, TouchableOpacity } from 'react-native'
-import _ from 'underscore'
 import moment from 'moment'
 import styles from './styles'
 
